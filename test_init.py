9ae0507701ef8a5b06b48a4ad76c7c5cb201a5ee
maitray-agrawal <maitrayagrawal1705@gmail.com>
2026-06-05 16:31:24 +0530

    Remove obsolete files

--- test_init.py
+++
@@ -1,9 +0,0 @@
-from database import init_db
-from data_engine import logger
-
-if __name__ == "__main__":
-    try:
-        init_db()
-        logger.info("Database initialization test successful.")
-    except Exception as e:
-        logger.critical(f"Database initialization failed: {e}")
